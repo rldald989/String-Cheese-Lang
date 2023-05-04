92142d2f13427042c4667a5a5e516551a7e414f1
rldald989 <70861085+rldald989@users.noreply.github.com>
2023-05-04 19:54:01 +0000

    Added printl, readl, and strings

--- headers/Interpreter.hpp
+++ headers/Interpreter.hpp
@@ -1,118 +1,188 @@
 #pragma once
 
-#include "Lexer.hpp"
-
+#include "Parser.hpp"
 namespace strc{
     class Interpreter
     {
     private:
-        std::vector<strc::INT> ints;
-        std::vector<strc::FLOAT> floats;
-        std::vector<strc::STRING> strings;
-        std::vector<std::string>& identifiers;
-        Lexer lex;
-        Logger log;
+        Parser m_parse;
     public:
-        Interpreter(std::string file_name) : lex(file_name), identifiers(lex.GetIdentifiers()){}
+        Interpreter(std::string file_name) : m_parse(file_name){}
         ~Interpreter(){
-        }
-
-        void CheckInts(int i)
-        {
-            if(identifiers[i] == "int")
-            {
-                if(identifiers[i + 2] == ":")
-                {
-                    ints.push_back(strc::MakeInt(std::stoi(identifiers[i + 3]), identifiers[i + 1]));
-                }
-                else
-                {
-                    std::cout << "SYNTAX ERROR AT: " << identifiers[i] << " "  << identifiers[i + 1] << std::endl;
-                }
-
-                if(ints[i].value > 2147483647)
-                {
-                    log.Error(std::string(identifiers[i] + " " + identifiers[i + 1]).c_str(), " is too large, program crashed");
-                }
-            }
-        }
-
-        void CheckFloats(int i)
-        {
-            if(identifiers[i] == "float")
-            {
-                if(identifiers[i + 2] == ":")
-                {
-                    floats.push_back(strc::MakeFloat(std::stof(identifiers[i + 3]), identifiers[i + 1]));
-                }
-                else
-                {
-                    std::cout << "SYNTAX ERROR AT: " << identifiers[i] << " "  << identifiers[i + 1] << std::endl;
-                }
-            }
-        }
-
-        void CheckStrings(int i)
-        {
-            if(identifiers[i] == "string")
-            {
-                if(identifiers[i + 2] == ":")
-                {
-                    if(identifiers[i + 3] == "\"")
-                    {
-                        strings.push_back(strc::MakeString(identifiers[i + 3], identifiers[i + 1]));
-                    }
-                }
-                else
-                {
-                    std::cout << "SYNTAX ERROR AT: " << identifiers[i] << " "  << identifiers[i + 1] << std::endl;
-                }
-            }
         }
 
         void Print(int i)
         {
-            if(identifiers[i] == "print")
+            std::vector<std::string>& id = m_parse.GetIdentifiers();
+            std::vector<strc::INT*> ints = m_parse.GetInts();
+            std::vector<strc::FLOAT*> floats = m_parse.GetFloats();
+            std::vector<strc::STRING*> strings = m_parse.GetStrings();
+            if(m_parse.GetIdentifiers()[i] == "print")
             {
-                if(identifiers[i + 1] == ":")
+                if(id[i + 1] == ":")
                 {
                     for(int j = 0; j < ints.size(); j++)
                     {
-                        if(identifiers[i + 2] == ints[j].name)
+                        if(id[i + 2] == ints[j]->name)
                         {
-                            std::cout << ints[j].value << std::endl;
+                            std::cout << ints[j]->value;
                         }
                     }
 
                     for(int j = 0; j < floats.size(); j++)
                     {
-                        if(identifiers[i + 2] == floats[j].name)
+                        if(id[i + 2] == floats[j]->name)
                         {
-                            std::cout << floats[j].value << std::endl;
+                            std::cout << floats[j]->value;
                         }
                     }
 
                     for(int j = 0; j < strings.size(); j++)
                     {
-                        if(identifiers[i + 2] == ints[j].name)
+                        if(id[i + 2] == strings[j]->name)
                         {
-                            std::cout << strings[j].value << std::endl;
+                            std::cout << strings[j]->value;
                         }
                     }
                 }
                 else{
-                    std::cout << "SYNTAX ERROR AT: " << identifiers[i] << " "  << identifiers[i + 1] << std::endl;
+                    std::cout << "SYNTAX ERROR AT: " << id[i] << " "  << id[i + 1] << std::endl;
+                }
+            }
+        }
+
+        void PrintLine(int i)
+        {
+            std::vector<std::string>& id = m_parse.GetIdentifiers();
+            std::vector<strc::INT*> ints = m_parse.GetInts();
+            std::vector<strc::FLOAT*> floats = m_parse.GetFloats();
+            std::vector<strc::STRING*> strings = m_parse.GetStrings();
+            if(m_parse.GetIdentifiers()[i] == "printl")
+            {
+                if(id[i + 1] == ":")
+                {
+                    for(int j = 0; j < ints.size(); j++)
+                    {
+                        if(id[i + 2] == ints[j]->name)
+                        {
+                            std::cout << ints[j]->value << std::endl;
+                        }
+                    }
+
+                    for(int j = 0; j < floats.size(); j++)
+                    {
+                        if(id[i + 2] == floats[j]->name)
+                        {
+                            std::cout << floats[j]->value << std::endl;
+                        }
+                    }
+
+                    for(int j = 0; j < strings.size(); j++)
+                    {
+                        if(id[i + 2] == strings[j]->name)
+                        {
+                            std::cout << strings[j]->value << std::endl;
+                        }
+                    }
+                }
+                else{
+                    std::cout << "SYNTAX ERROR AT: " << id[i] << " "  << id[i + 1] << std::endl;
+                }
+            }
+        }
+
+        void Read(int i)
+        {
+            std::vector<std::string>& id = m_parse.GetIdentifiers();
+            std::vector<strc::INT*> ints = m_parse.GetInts();
+            std::vector<strc::FLOAT*> floats = m_parse.GetFloats();
+            std::vector<strc::STRING*> strings = m_parse.GetStrings();
+            if(m_parse.GetIdentifiers()[i] == "read")
+            {
+                if(id[i + 1] == ":")
+                {
+                    for(int j = 0; j < ints.size(); j++)
+                    {
+                        if(id[i + 2] == ints[j]->name)
+                        {
+                            std::cin >> ints[j]->value;
+                        }
+                    }
+
+                    for(int j = 0; j < floats.size(); j++)
+                    {
+                        if(id[i + 2] == floats[j]->name)
+                        {
+                            std::cin >> floats[j]->value;
+                        }
+                    }
+
+                    for(int j = 0; j < strings.size(); j++)
+                    {
+                        if(id[i + 2] == strings[j]->name)
+                        {
+                            std::cin >> strings[j]->value;
+                        }
+                    }
+                }
+                else{
+                    std::cout << "SYNTAX ERROR AT: " << id[i] << " "  << id[i + 1] << std::endl;
+                }
+            }
+        }
+
+        void ReadLine(int i)
+        {
+            std::vector<std::string>& id = m_parse.GetIdentifiers();
+            std::vector<strc::INT*> ints = m_parse.GetInts();
+            std::vector<strc::FLOAT*> floats = m_parse.GetFloats();
+            std::vector<strc::STRING*> strings = m_parse.GetStrings();
+            if(m_parse.GetIdentifiers()[i] == "readl")
+            {
+                if(id[i + 1] == ":")
+                {
+                    for(int j = 0; j < strings.size(); j++)
+                    {
+                        if(id[i + 2] == strings[j]->name)
+                        {
+                            std::getline(std::cin, strings[j]->value);
+                        }
+                    }
+                }
+                else{
+                    std::cout << "SYNTAX ERROR AT: " << id[i] << " "  << id[i + 1] << std::endl;
+                }
+            }
+        }
+
+        bool CheckStandard(int i)
+        {
+            std::vector<std::string>& id = m_parse.GetIdentifiers();
+            if(m_parse.GetIdentifiers()[i] == "-bring")
+            {
+                if(id[i + 1] == "<standard>")
+                {
+                    return true;
+                }
+                else{
+                    return false;
+                    std::cout << "SYNTAX ERROR AT: " << id[i] << " "  << id[i + 1] << std::endl;
                 }
             }
         }
 
         void Run(){
-            for(int i = 0; i < identifiers.size(); i++)
+            for(int i = 0; i < m_parse.GetIdentifiers().size(); i++)
             {
                 //std::cout << identifiers[i] << std::endl;
-                CheckInts(i);
-                CheckFloats(i);
+                m_parse.CheckInts(i);
+                m_parse.CheckFloats(i);
+                m_parse.CheckStrings(i);
                 Print(i);
+                PrintLine(i);
+                Read(i);
+                ReadLine(i);
+                //PrintIdentifiers();
             }
         }
     };
---
+++ headers/Parser.hpp
@@ -0,0 +1,118 @@
+#pragma once
+
+#include "Lexer.hpp"
+#include <string>
+#include <algorithm>
+
+namespace strc{
+    class Parser
+    {
+    private:
+        std::vector<strc::INT*> ints;
+        std::vector<strc::FLOAT*> floats;
+        std::vector<strc::STRING*> strings;
+        std::vector<std::string>& identifiers;
+        Lexer lex;
+        Logger log;
+    public:
+        Parser(std::string file_name): lex(file_name), identifiers(lex.GetIdentifiers()){}
+        ~Parser(){}
+
+        void RemoveCharacter(std::string* strToRemove, char charToRemove)
+        {
+            strToRemove->erase(remove(strToRemove->begin(), strToRemove->end(), charToRemove), strToRemove->end());
+        }
+
+        void CheckInts(int i)
+        {
+            if(identifiers[i] == "int")
+            {
+                if(identifiers[i + 2] == ":")
+                {
+                    ints.push_back(new INT(strc::MakeInt(std::stoi(identifiers[i + 3]), identifiers[i + 1])));
+                }
+                else
+                {
+                    std::cout << "SYNTAX ERROR AT: " << identifiers[i] << " "  << identifiers[i + 1] << std::endl;
+                }
+
+                if(ints[i]->value > 2147483647)
+                {
+                    log.Error(std::string(identifiers[i] + " " + identifiers[i + 1]).c_str(), " is too large, program crashed");
+                }
+            }
+        }
+
+        void CheckFloats(int i)
+        {
+            if(identifiers[i] == "float")
+            {
+                if(identifiers[i + 2] == ":")
+                {
+                    floats.push_back(new FLOAT(strc::MakeFloat(std::stof(identifiers[i + 3]), identifiers[i + 1])));
+                }
+                else
+                {
+                    std::cout << "SYNTAX ERROR AT: " << identifiers[i] << " "  << identifiers[i + 1] << std::endl;
+                }
+            }
+        }
+
+        void CheckStrings(int i)
+        {
+            int iterator = 0;
+            if(identifiers[i] == "string")
+            {
+                if(identifiers[i + 2] == ":")
+                {
+                    std::string quoteCheck = identifiers[i + 3].substr(0, 1);
+                    if(quoteCheck == "\"")
+                    {
+                        RemoveCharacter(&identifiers[i + 3], '\"');
+                        std::string temp = identifiers[i + 3];
+                        
+                        while(identifiers[i + 3 + iterator].back() != '\"')
+                        {
+                            temp += " " + identifiers[i + 3 + iterator + 1];
+                            RemoveCharacter(&temp, '\"');
+                            iterator++;
+                        }
+                        
+                        strings.push_back(new STRING(strc::MakeString(temp, identifiers[i + 1])));
+                    }
+                }
+                else
+                {   
+                    std::cout << "SYNTAX ERROR AT: " << identifiers[i] << " "  << identifiers[i + 1] << std::endl;
+                }
+            }
+        }
+
+        std::vector<strc::INT*> GetInts()
+        {
+            return ints;
+        }
+
+        std::vector<strc::FLOAT*> GetFloats()
+        {
+            return floats;
+        }
+
+        std::vector<strc::STRING*> GetStrings()
+        {
+            return strings;
+        }
+
+        void PrintIdentifiers(){
+            for(int i = 0; i < identifiers.size(); i++){
+                std::cout << identifiers[i] << std::endl;
+            }
+        }
+
+        std::vector<std::string>& GetIdentifiers()
+        {
+            return lex.GetIdentifiers();
+        }
+
+    };
+}
